1267dd894be28f9048fb98839de497dce4d5ddc5
Edmilson <44274029+EdmilsonOSJr@users.noreply.github.com>
2021-04-08 02:53:23 -0300

    add operação de remoção de contato

--- script.js
+++ script.js
@@ -1,19 +1,46 @@
+const nomeChave = "formulario";
+
 $(function($){
 
-    let listaContatos = recuperaListaContatos();
+    let listaContatos = recuperaListaContatos(nomeChave);
 
     if(!isEmpty(listaContatos)){
         for(let i=0 ; i<listaContatos.length; i++){
-            appendLine(listaContatos[i].nome,listaContatos[i].telefone,listaContatos[i].cidade,listaContatos[i].estado,listaContatos[i].email,listaContatos[i].informacoes,listaContatos[i].categoria);
+            appendLine(listaContatos[i].id,listaContatos[i].nome,listaContatos[i].telefone,
+                        listaContatos[i].cidade,listaContatos[i].estado,listaContatos[i].email,
+                        listaContatos[i].informacoes,listaContatos[i].categoria);
         }
     }
+
+    
+    $('.clicado').click(function () {
+        id = $(this).parents('tr').find('th').eq(0).text();
+            
+        removerContato(id, this);
+    
+    });
+
 });
 
+function removerContato(id, elemento){
+    
+    elemento.closest("tr").remove();
+
+    let listaContatos = recuperaListaContatos(nomeChave), listaNova = [];
+
+    for(let i=0 ; i<listaContatos.length; i++){
+        if(id!=listaContatos[i].id)
+            listaNova.push(listaContatos[i]);
+    }
+
+    gravarContatos(listaNova, nomeChave);
+
+}
 
 function addContato(){
     
 
-    let listaContatos = recuperaListaContatos();
+    let listaContatos = recuperaListaContatos(nomeChave);
     
     let data = isEmpty(listaContatos)==true?[]:listaContatos;
     
@@ -32,8 +59,8 @@ function addContato(){
     
     if(!contatoExistente(formInfo.nome)){
         data.push(formInfo);
-        localStorage.setItem("formulario",JSON.stringify(data));
-        appendLine(formInfo.nome,formInfo.telefone,formInfo.cidade,formInfo.estado,formInfo.email,formInfo.informacoes,formInfo.categoria);
+        gravarContatos(data, nomeChave);
+        appendLine(id,formInfo.nome,formInfo.telefone,formInfo.cidade,formInfo.estado,formInfo.email,formInfo.informacoes,formInfo.categoria);
     }
     else
         alert("Contato Existente.")
@@ -41,9 +68,10 @@ function addContato(){
 };    
 
 
-function appendLine(nome, telefone, cidade, estado, email, informacoes, categoria){
-
+function appendLine(id, nome, telefone, cidade, estado, email, informacoes, categoria){
+    console.log(id);
     $("#contatos").append("<tr>"+
+                          "<th>"+id+"</th>"+
                           "<td>"+nome+"</td>"+
                           "<td>"+telefone+"</td>"+
                           "<td>"+cidade+"</td>"+
@@ -51,6 +79,9 @@ function appendLine(nome, telefone, cidade, estado, email, informacoes, categori
                           "<td>"+email+"</td>"+
                           "<td>"+informacoes+"</td>"+
                           "<td>"+categoria+"</td>"+
+                          "<td><button type=\"button\" class=\"clicado\" id=\"remover\">"+
+                          "Remover"+
+                          "</button></td>"+
                           "</tr>");
 
 }
@@ -67,8 +98,7 @@ function isEmpty(obj) {
 
 
 function contatoExistente(nome){
-    
-    let formInfo = recuperaListaContatos();
+    let formInfo = recuperaListaContatos(nomeChave);
 
     if(!isEmpty(formInfo)){
         for(let i=0 ; i<formInfo.length; i++){
@@ -78,16 +108,17 @@ function contatoExistente(nome){
     }
     
     return false;    
-
 }
 
 
-function recuperaListaContatos(){
-    return JSON.parse(localStorage.getItem("formulario"));
+function recuperaListaContatos(keyName){
+    return JSON.parse(localStorage.getItem(keyName));
+}
+
+function gravarContatos(contatos, keyName){
+    localStorage.setItem(keyName,JSON.stringify(contatos));
 }
 
 function idNovoContato(data){
-    
     return data.length>0?data[data.length-1].id+1:0;
-
 }
